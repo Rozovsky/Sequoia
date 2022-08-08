9b2f0cbca7b8c1c1a08384131eb5a2e9e8e4b797
v.baikov <amarelle.ua@gmail.com>
2022-08-08 17:22:42 +0300

    Samples authentication basic location generator

--- samples/Authentication/Basic/Samples.Authentication.Basic.Core/Infrastructure/Repositories/LocationRepository.cs
+++ samples/Authentication/Basic/Samples.Authentication.Basic.Core/Infrastructure/Repositories/LocationRepository.cs
@@ -6,13 +6,22 @@ namespace Samples.Authentication.Basic.Core.Infrastructure.Repositories
 {
     public class LocationRepository : ILocationRepository
     {
+        private readonly List<Location> _locations;
+
+        public LocationRepository()
+        {
+            // seed static data
+            _locations = ApplicationDbContextSeed.Seed();
+        }
+
         public async Task<List<Location>> GetLocations(CancellationToken cancellationToken)
         {
             await Task.Delay(0, cancellationToken);
 
-            return new List<Location>();
+            return _locations;
         }
 
+        // TODO: add mapings and paged processing
         public async Task<PagedWrapper<Location>> GetLocationsPaged(int page, int limit, CancellationToken cancellationToken)
         {
             await Task.Delay(0, cancellationToken);
